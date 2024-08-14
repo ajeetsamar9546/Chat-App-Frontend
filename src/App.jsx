1653e9bf8e8195c446fb4879dcde67437120549f
Ajeet Kumar <ajeetsamar9546@gmail.com>
2024-08-14 15:16:19 +0530

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('https://chatapp-backend-mddd.onrender.com'); // Connect to the back-end server
+
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.off('message');
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (message.trim()) {
+      socket.emit('message', message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+};
+
+export default App;
